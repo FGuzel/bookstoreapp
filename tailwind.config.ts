a337404722ec2d4868c47f58dcfbb66d5f80170c
Fatmanur GUZEL <fatmanur.guzel@akinon.com>
2023-12-27 19:54:52 +0300

    Added new features to Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,38 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      maxWidth: {
+        layout: '75rem'
+      },
+      width: {
+        '1/10': '10%',
+        '2/10': '20%',
+        '3/10': '30%',
+        '4/10': '40%',
+        '5/10': '50%',
+        '6/10': '60%',
+        '7/10': '70%',
+        '8/10': '80%',
+        '9/10': '90%',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '0rem'
+        }
+      },
+      screens: {
+        xs: '575px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1200px',
+      }
     },
   },
-  plugins: [],
+  plugins: []
 }
+
 export default config
