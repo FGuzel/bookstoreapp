c91995dc2c96b30b8391cf66b6db0a2a5fce56c0
Fatmanur GUZEL <fatmanur.guzel@akinon.com>
2023-12-27 20:01:35 +0300

    Created header with styling

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,5 +1,22 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.css";
+
 const Header = () => {
-    return <div>Header</div>
-}
+    return (
+        <header className={styles.header}>
+            <Link
+                href='/'
+                className={styles.logo}
+            >
+                BOOKSTORE
+            </Link>
+
+            <button>
+                SEARCH
+            </button>
+        </header>
+    );
+};
 
 export default Header;
