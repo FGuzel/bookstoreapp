988292e8f11ab67566acea9b6c10c2ce1c847cd6
Fatmanur GUZEL <fatmanur.guzel@akinon.com>
2023-12-27 20:37:23 +0300

    Update next config to fetch images from Google Books API

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "books.google.com"
+            },
+            {
+                protocol: "http",
+                hostname: "localhost"
+            }
+        ]
+    },
+    modularizeImports: {
+        components: {
+          transform: 'components/{{ kebabCase member }}',
+          skipDefaultConversion: true
+        }
+    },
     env: {
         USER_ID: process.env.USER_ID,
         SHELF_ID: process.env.SHELF_ID,
-    }
+    },
 }
 
 module.exports = nextConfig
