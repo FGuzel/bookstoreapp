3903bed5ba2793aa49c950d7cc082d1f38319e8f
Fatmanur GUZEL <fatmanur.guzel@akinon.com>
2023-12-27 20:12:37 +0300

    Implemented API to fetch books from user x's bookshelf on Google Books and added necessary keys in env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    env: {
+        USER_ID: process.env.USER_ID,
+        SHELF_ID: process.env.SHELF_ID,
+    }
+}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,65 @@
-export default function Home() {
-  return <div>Main</div>
+'use client';
+import React, { useEffect, useState } from 'react';
+
+type BookType = {
+  id: number;
+  volumeInfo: {
+      title: string;
+      authors: string[];
+      categories: string[];
+      imageLinks: {
+          thumbnail: string;
+      };
+  },
+  saleInfo: {
+      listPrice: {
+          amount: number;
+      },
+      retailPrice: {
+          amount: number;
+      }
+  }
 }
+
+async function fetchBooks() {
+  const params = new URLSearchParams({
+    maxResults: '20',
+    startIndex: '0',
+  });
+  const userId = process.env.USER_ID;
+  const shelfId = process.env.SHELF_ID;
+
+  const url = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${shelfId}/volumes?${params}`;
+
+  try {
+    const response = await fetch(url);
+    const { items = [] } = await response.json();
+    return items;
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    return [];
+  }
+}
+
+const Home = () => {
+  const [books, setBooks] = useState<BookType[]>([]);
+
+  useEffect(() => {
+    const fetchBooksData = async () => {
+      const booksData = await fetchBooks();
+      setBooks(booksData);
+    };
+
+    fetchBooksData();
+  }, []);
+
+  return (
+    <div>
+      {books.map((book, bookIndex) => (
+        <div key={bookIndex}>{book.id}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
